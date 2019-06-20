a8682845369252a9915a33d6b70e5e4582843c74
Alexey Efimov <lex.efimov@gmail.com>
2019-06-20 16:53:49 +0500

    Update User Interface for Question View Controller

---
+++ Personal Quiz/Controller/QuestionViewController.swift
@@ -0,0 +1,105 @@
+//
+//  QuestionViewController.swift
+//  Personal Quiz
+//
+//  Created by Alexey Efimov on 20/06/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    // MARK: IB Outlets
+    @IBOutlet var questionLable: UILabel!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleButton: UIButton!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedButton: UIButton!
+    
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    // MARK: Pivate Properties
+    private var questions = Question.getQuestions()
+    private var questionIndex = 0 // Index of current question
+
+}
+
+// MARK: UIViewController Methods
+extension QuestionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // update user interface
+        updateUI()
+    }
+}
+
+// MARK: User Interface
+extension QuestionViewController {
+    // Updates user interface
+    private func updateUI() {
+        // hide everything
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        // get current question
+        let currentQuestion = questions[questionIndex]
+        
+        // get current answer
+        let currentAnswers = currentQuestion.answers
+        
+        // calculate progress
+        let totalProgress = Float(questionIndex / questions.count)
+        
+        // set progress for questionProgressView
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        // set navigation title
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        // show stack view corresponding to question type
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStackView(using: currentAnswers)
+        case .multiple:
+            updateMultipleStackView(using: currentAnswers)
+        case .ranged:
+            updateRangedStackView(using: currentAnswers)
+        }
+    }
+    
+    /// Setup single stack view
+    ///
+    /// - Parameter answers: [Answer] array with answers
+    private func updateSingleStackView(using answers: [Answer]) {
+        // show single satck view
+        singleStackView.isHidden = false
+    }
+    
+    /// Setup multiple stack view
+    ///
+    /// - Parameter answers: [Answer] array with answers
+    private func updateMultipleStackView(using answers: [Answer]) {
+        // show multiple satck view
+        multipleStackView.isHidden = false
+    }
+    
+    /// Setup ranged stack view
+    ///
+    /// - Parameter answers: [Answer] array with answers
+    private func updateRangedStackView(using answers: [Answer]) {
+        // show ranged satck view
+        rangedStackView.isHidden = false
+    }
+}
+
---
+++ Personal Quiz/Controller/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  Personal Quiz
+//
+//  Created by Alexey Efimov on 20/06/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
